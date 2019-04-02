108ed9f0aea35518de3b54b169ec6cc1757767cc
Adrian <a.fernandez1@live.com>
2019-04-02 14:42:03 -0700

    webpack-dev-server installed

---
+++ index.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+
+console.log('Hello! From index.js!');
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+var name = "Adrian", time = "today";
+console.log(`Yo!! ${name}! How are ya ${time} man?`);
